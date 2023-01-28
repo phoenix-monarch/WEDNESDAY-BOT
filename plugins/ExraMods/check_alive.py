486b3f162c92eeb8f8cb037b5d43e6e25fa71469
ɴᴧʀᴜᴛᴏ ᴜᴢᴜᴍᴀᴋɪ <111524889+phoenix-monarch@users.noreply.github.com>
2023-01-28 10:50:18 +0530

    Update check_alive.py

--- plugins/ExraMods/check_alive.py
+++ plugins/ExraMods/check_alive.py
@@ -6,7 +6,7 @@ CMD = ["/", "."]
 
 @Client.on_message(filters.command("alive", CMD))
 async def check_alive(_, message):
-    await message.reply_text("ചത്തിട്ടില്ല മുത്തേ ഇവിടെ തന്നെ ഉണ്ട്.. നിനക്ക് ഇപ്പൊ എന്നോട് ഒരു സ്നേഹവും ഇല്ല. കൊള്ളാം.. നീ പാഴെ പോലെയേ അല്ല മാറിപോയി..😔 ഇടക്ക് എങ്കിലും ചുമ്മാ ഒന്ന് /start ചെയ്തു നോക്ക്..🙂")
+    await message.reply_text("Not dead but still here.. You have no love for me now.  Good.. You are not the same as you were before..use /start")
 
 
 @Client.on_message(filters.command("ping", CMD))
